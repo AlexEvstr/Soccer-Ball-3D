4f0b1b58a5d13ad6c9f64358fa5ba086389855bf
Alexander Evstratov <alexdrugev@me.com>
2023-03-30 13:46:39 +0300

    Changed jump mechanic.

--- Assets/_Scripts/Player/Jump.cs
+++ Assets/_Scripts/Player/Jump.cs
@@ -5,17 +5,28 @@ public class Jump : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     private float _jumpForce = 150.0f;
+    public bool _isGround;
 
     private void Start()
     {
+        _isGround = false;
         _rigidbody = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && _isGround)
         {
             _rigidbody.AddForce(0, _jumpForce, 0);
+            _isGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isGround = true;
         }
     }
 }
